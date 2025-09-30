b1db82d96a88db2802bb28539d1c4fc7f7a9a2ae
Dorian Michaux <dorianmichaux@mac.home>
2025-09-30 11:38:11 +0200

    update DB : add table member_id_check

--- backend/DBScript/CreateDB.sql
+++ backend/DBScript/CreateDB.sql
@@ -8,49 +8,58 @@
 
 /* docker run my-postgres-container */
 
-DROP TABLE IF EXISTS user_role CASCADE;
-CREATE TABLE user_role(
+DROP TABLE IF EXISTS member_role CASCADE;
+CREATE TABLE member_role(
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL
 );
 
-DROP TABLE IF EXISTS user_2fa CASCADE;
-CREATE TABLE user_2fa(
+DROP TABLE IF EXISTS member_2fa CASCADE;
+CREATE TABLE member_2fa(
     id SERIAL PRIMARY KEY,
     secret_key varchar(100) NOT NULL,
     is_enabled BOOLEAN NOT NULL DEFAULT FALSE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-DROP TABLE IF EXISTS user CASCADE;
-CREATE TABLE user (
+DROP TABLE IF EXISTS member_id_check CASCADE;
+CREATE TABLE member_id_check(
+    id SERIAL PRIMARY KEY,
+    card_front_id varchar(50) NOT NULL,
+    card_back_id varchar(50) NOT NULL,
+    reject_reason VARCHAR(200), 
+)
+
+DROP TABLE IF EXISTS member CASCADE;
+CREATE TABLE member (
     id SERIAL PRIMARY KEY,
     first_name varchar(50) NOT NULL,
     last_name varchar(50) NOT NULL,
     email varchar(100) UNIQUE NOT NULL,
-    is_active BOOLEAN DEFAULT FALSE,
+    email_checked BOOLEAN DEFAULT FALSE,
+    id_checked BOOLEAN DEFAULT FALSE,
     password VARCHAR(255) NOT NULL,
     password_last_update TIMESTAMP,
     address varchar(100) NOT NULL,
     birthday DATE NOT NULL,
     national_registry VARCHAR(20) UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    id_role INTEGER NOT NULL REFERENCES user_role(id),
-    id_user_2fa INTEGER NOT NULL REFERENCES user_2fa(id)
+    id_role INTEGER NOT NULL REFERENCES member_role(id),
+    id_member_2fa INTEGER NOT NULL REFERENCES member_2fa(id)
 );
 
-DROP TABLE IF EXISTS user_group CASCADE;
-CREATE TABLE user_group(
+DROP TABLE IF EXISTS member_group CASCADE;
+CREATE TABLE member_group(
     id SERIAL PRIMARY KEY,
-    id_admin INTEGER NOT NULL REFERENCES user(id),
+    id_admin INTEGER NOT NULL REFERENCES member(id),
     name varchar(50) NOT NULL
 );
 
 DROP TABLE IF EXISTS member_group CASCADE;
 CREATE TABLE member_group(
     id SERIAL PRIMARY KEY,
-    id_group INTEGER NOT NULL REFERENCES user_group(id),
-    id_user INTEGER NOT NULL REFERENCES user(id)
+    id_group INTEGER NOT NULL REFERENCES member_group(id),
+    id_member INTEGER NOT NULL REFERENCES member(id)
 );
 
 DROP TABLE IF EXISTS type_danger CASCADE;
@@ -71,10 +80,10 @@ CREATE TABLE report(
     id_type_danger INTEGER NOT NULL  REFERENCES type_danger(id)
 );
 
-DROP TABLE IF EXISTS user_report CASCADE;
-CREATE TABLE user_report(
+DROP TABLE IF EXISTS member_report CASCADE;
+CREATE TABLE member_report(
     id SERIAL PRIMARY KEY,
-    id_user INTEGER NOT NULL REFERENCES user(id),
+    id_member INTEGER NOT NULL REFERENCES member(id),
     id_report INTEGER NOT NULL REFERENCES report(id)
 );
 
@@ -84,6 +93,6 @@ CREATE TABLE validation_code(
     code_hash VARCHAR(100) NOT NULL,
     expires_at DATE NOT NULL,
     attempts INTEGER NOT NULL,
-    id_user INTEGER NOT NULL REFERENCES user(id)
+    id_member INTEGER NOT NULL REFERENCES member(id)
 );
 
---
+++ backend/DBScript/test.sql
@@ -0,0 +1,16 @@
+select * from product;
+
+{
+    "Client" : {
+        "name" : "aaaaaa",
+        "firstname" : "bbbb",
+        "address" : "ccccc",
+        "email" : "dsqdsqdsqds@ddsqdq.com",
+        "password" : "password"
+    },
+
+    "purchase" : {
+        "articleID" : 1,
+        "quantity" : 2
+    }
+}
