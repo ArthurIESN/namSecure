2dc5c257d702047dc3969dfc569726ca8ade1bf1
ArthurIESN <ryckbosch.arthur.iesn@gmail.com>
2025-10-11 22:20:17 +0200

    updated server

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,5 @@
 import './utils/logs/enhancedLogs.js';
 
-// JUST FOR TEST
-// SEND AN EMAIL
-import { sendEmail } from './utils/email/email.js';
-
-
 import express from 'express';
 import router from './routes/router.js';
 
@@ -18,13 +13,5 @@ app.listen(PORT, () => {
 
 
     console.log(`Server running on port ${PORT}`);
-    try
-    {
-        sendEmail("ryckbosch.arthur@gmail.com", "test from backend", "text content", "<h1>html content</h1>");
-    }
-    catch (error)
-    {
-        console.error("Failed to send test email:", error);
-    }
 
 });
