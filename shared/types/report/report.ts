0eaa5c3cccb0dc5ae82b95c2ffdb4370fd7f01a3
AirBreakerPictures <150797257+AirBreakerPictures@users.noreply.github.com>
2025-11-05 21:30:44 +0100

    backoffice part 2

--- shared/types/report/report.ts
+++ shared/types/report/report.ts
@@ -1,12 +1,15 @@
 import type {IPoint} from "../point/point.js";
 import type  {ITypeDanger} from "../type_danger/type_danger.js";
+import type {IMember} from "../member/member.js";
 
 export interface IReport {
    id : number;
    date : Date;
-   desciption : string;
-   location : IPoint;
+   lat : number;
+   lng : number;
+   street : string;
    level : number;
-   photo_id : string;
-   id_type_danger : ITypeDanger;
+   photo_path: string;
+   member : IMember;
+   type_danger : ITypeDanger;
 }
