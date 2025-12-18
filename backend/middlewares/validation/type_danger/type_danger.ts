13861e5de3a6343782637363d5cceea038119b62
AirBreakerPictures <150797257+AirBreakerPictures@users.noreply.github.com>
2025-12-19 00:42:00 +0100

    fix todo

--- backend/middlewares/validation/type_danger/type_danger.ts
+++ backend/middlewares/validation/type_danger/type_danger.ts
@@ -8,11 +8,7 @@ const typeDangersSchema = vine.object({
     search: vine.string().minLength(0).maxLength(100).optional(),
 });
 
-const typeDangersUsedSchema = vine.object({
-    limit: vine.number().positive().withoutDecimals().max(GET_MAX_LIMIT),
-    offset: vine.number().nonNegative().withoutDecimals(),
-    search: vine.string().minLength(0).maxLength(100).optional(),
-});
+const typeDangersUsedSchema = vine.object({});
 
 const typeDangerSchema = vine.object({
     id: vine.number().positive().withoutDecimals(),
